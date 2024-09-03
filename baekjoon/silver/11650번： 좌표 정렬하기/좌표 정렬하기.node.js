b112b42d1838ed277ca952c565584b48886e565b
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2024-09-03 17:11:29 +0900

    BOJ-EX: 2024. 9. 3. 오후 5:11:28

--- baekjoon/silver/11650번： 좌표 정렬하기/좌표 정렬하기.node.js
+++ baekjoon/silver/11650번： 좌표 정렬하기/좌표 정렬하기.node.js
@@ -1,12 +1,33 @@
-undefined ************************************************************************** undefined
-undefined                                                                            undefined
-undefined                                               :::    :::    :::            undefined
-undefProblem Number: 11650                             :+:    :+:      :+:           undefined
-undefined                                             +:+    +:+        +:+          undefined
-undefBy: ghwns8893 <boj.kr/u/ghwns8893>              +#+    +#+          +#+         undefined
-undefined                                           +#+      +#+        +#+          undefined
-undefhttps://boj.kr/11650                          #+#        #+#      #+#           undefined
-undefSolved: 2024/09/03 15:39:20 by ghwns8893     ###          ###   ##.kr           undefined
-undefined                                                                            undefined
-undefined ************************************************************************** undefined
+// 실버 5 - 좌표 정렬하기
+// sort => 배열을 정렬
 
+const fs = require("fs");
+let input = fs
+  .readFileSync("/Users/jihojun/vscode/algorithm/input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const iter = Number(input.shift());
+let arr = [];
+
+for (let i = 0; i < iter; i++) {
+  arr.push(input[i].split(" ").map((item) => Number(item)));
+}
+
+arr.sort((a, b) => {
+  if (a[0] === b[0]) {
+    return a[1] - b[1];
+  } else {
+    return a[0] - b[0];
+  }
+});
+
+let ans = [];
+
+for (let i = 0; i < iter; i++) {
+  let answer = arr[i].join(" ");
+  ans.push(answer);
+}
+
+console.log(ans.join("\n"));
