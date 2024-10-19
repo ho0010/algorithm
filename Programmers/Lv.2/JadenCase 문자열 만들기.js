f1a67a9703987f161fb42b41ea7422e2ae76fc06
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2024-10-19 17:13:31 +0900

    solve

---
+++ Programmers/Lv.2/JadenCase 문자열 만들기.js
@@ -0,0 +1,33 @@
+// c+o+n
+
+solution("3people unFollowed me");
+
+function solution(s) {
+  var answer = "";
+
+  function isUpperCase(char) {
+    return char === char.toUpperCase();
+  }
+
+  const firstString = s[0];
+  const changedFirstString = firstString.toUpperCase();
+
+  answer = changedFirstString + s.slice(1);
+
+  let answerArray = answer.split("");
+
+  for (i = 1; i < s.length; i++) {
+    if (s[i] == " " && i + 1 < s.length) {
+      let temp = s[i + 1].toUpperCase();
+      answerArray[i + 1] = temp;
+    } else if (isUpperCase && s[i - 1] != " ") {
+      answerArray[i] = s[i].toLowerCase();
+    }
+  }
+  answer = answerArray.join("");
+  console.log(answer);
+
+  return answer;
+}
+
+// 문자열 => 배열 => 문자열
