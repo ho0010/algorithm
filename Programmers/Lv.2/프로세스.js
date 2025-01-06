402c76b4c562147c6e5aec59503c9ce19b2b4c47
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2025-01-06 16:33:22 +0900

    다리를 지나는 트럭 정리

---
+++ Programmers/Lv.2/프로세스.js
@@ -0,0 +1,49 @@
+function solution(priorities, location) {
+  var answer = 0;
+  let answerArray = [];
+  let length = priorities.length;
+  let j = 0;
+  let countZero = 0;
+  let updatedArray = priorities;
+
+  for (let i = 0; i < length; i++) {
+    let temp = updatedArray[j];
+
+    if (countZero == length) {
+      break;
+    }
+    if (temp == 0) {
+      i--;
+      j++;
+      j %= length;
+
+      continue;
+    }
+    let maxNumber = Math.max(...updatedArray);
+    let maxIndex = updatedArray.indexOf(maxNumber);
+
+    if (temp < maxNumber) {
+      updatedArray[maxIndex] = 0;
+      answerArray.push(maxIndex);
+      j = maxIndex;
+    } else {
+      updatedArray[j] = 0;
+      answerArray.push(j);
+    }
+    j++;
+    j %= length;
+    countZero++;
+  }
+  answer = answerArray.indexOf(location) + 1;
+
+  console.log(answer);
+  return answer;
+}
+
+solution([1, 2, 3, 4, 5], 2);
+// 큐에서 하나씩 빼는 기능
+// 빼낸 우선순위와 큐에 남은 다른 우선순위들을 비교해 더 큰 숫자가 있는지 확인하는 로직
+// 빼낸 우선순위의 숫자를 넣는게 아니라 위치를 정답 큐를 만들어서 넣는 기능
+// 주어진 location에 해당하는 값을 정답 큐에서 찾아서 return하는 기능
+
+//실패했는데 이유를 모르겠음..
--- Programmers/test.js
+++ Programmers/test.js
@@ -1,49 +1,22 @@
 function solution(priorities, location) {
-  var answer = 0;
-  let answerArray = [];
-  let length = priorities.length;
-  let j = 0;
-  let countZero = 0;
-  let updatedArray = priorities;
+  let queue = priorities.map((priority, index) => [priority, index]);
+  let answer = 0;
 
-  for (let i = 0; i < length; i++) {
-    let temp = updatedArray[j];
-    console.log(temp);
-    if (countZero == length) {
-      break;
-    }
-    if (temp == 0) {
-      i--;
-      j++;
-      j %= length;
-
-      continue;
-    }
-    let maxNumber = Math.max(...updatedArray);
-    let maxIndex = updatedArray.indexOf(maxNumber);
-
-    if (temp < maxNumber) {
-      updatedArray[maxIndex] = 0;
-      answerArray.push(maxIndex);
-      j = maxIndex;
+  while (queue.length > 0) {
+    let [currentPriority, currentIndex] = queue.shift();
+    if (queue.some(([priority]) => priority > currentPriority)) {
+      queue.push([currentPriority, currentIndex]);
     } else {
-      updatedArray[j] = 0;
-      answerArray.push(j);
+      answer++;
+      if (currentIndex === location) {
+        return answer;
+      }
     }
-    j++;
-    j %= length;
-    countZero++;
-    console.log(updatedArray);
-    console.log(answerArray);
   }
-  answer = answerArray.indexOf(location) + 1;
 
-  console.log(answer);
   return answer;
 }
 
-solution([2, 3, 3, 2, 9, 3, 3], 3);
-// 큐에서 하나씩 빼는 기능
-// 빼낸 우선순위와 큐에 남은 다른 우선순위들을 비교해 더 큰 숫자가 있는지 확인하는 로직
-// 빼낸 우선순위의 숫자를 넣는게 아니라 위치를 정답 큐를 만들어서 넣는 기능
-// 주어진 location에 해당하는 값을 정답 큐에서 찾아서 return하는 기능
+console.log(solution([1, 2, 3, 4, 5], 2));
+// 굳이 모든 요소를 정렬 시키는게 아니라
+// 구하는 것에 집중하면 구하는 것의 우선순위만 알아내면 되니까 그거에 집중해서 풀이
