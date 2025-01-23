9345df666926504102075340672159c7b6fe52cc
Ji Ho Jun <129824629+ho0010@users.noreply.github.com>
2025-01-23 14:53:05 +0900

    [Gold V] Title: 풍선 맞추기, Time: 1300 ms, Memory: 97720 KB -BaekjoonHub

---
+++ 백준/Gold/11509. 풍선 맞추기/풍선 맞추기.js
@@ -0,0 +1,29 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let N = Number(input[0]); 
+let balloons = input[1].split(' ').map(Number); 
+
+let arrows = []; 
+let answer = 0;
+
+for (let h of balloons) {
+  let found = false;
+
+  // 현재 풍선을 터뜨릴 수 있는 화살 찾기
+  for (let i = 0; i < arrows.length; i++) {
+    if (arrows[i] === h) {
+      arrows[i]--; // 해당 화살의 높이를 감소
+      found = true;
+      break;
+    }
+  }
+
+  if (!found) {
+    // 기존 화살로 터뜨릴 수 없으면 새 화살 추가
+    arrows.push(h - 1);
+    answer++;
+  }
+}
+
+console.log(answer);
