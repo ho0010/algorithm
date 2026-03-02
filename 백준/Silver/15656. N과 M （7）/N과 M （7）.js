c0304bbbb625eea7661e98ec1019c46c4ace698e
Ji Ho Jun <129824629+ho0010@users.noreply.github.com>
2026-03-02 14:58:45 +0900

    [Silver III] Title: N과 M (7), Time: 800 ms, Memory: 211636 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.js
@@ -0,0 +1,27 @@
+let input = require('fs').readFileSync('./dev/stdin').toString().trim().split('\n');
+let [N, M] = input[0].split(' ').map(Number);
+let arr = input[1].split(' ').map(Number);
+
+arr.sort((a, b) => a - b);
+
+let answer = [];
+let stack = [];
+
+function sol(k) {
+  if (stack.length === M) {
+    answer.push(stack.join(' '));
+    return;
+  }
+  for (let i = 0; i < N; i++) {
+    stack.push(arr[i]);
+    sol(i + 1);
+    stack.pop();
+  }
+}
+
+sol(0);
+
+console.log(answer.join('\n'));
+
+// 발산
+// 모든 경우의 수가 필요함
