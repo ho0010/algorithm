8f93f340a9af523a57f85b4e176e1901255fe9aa
Ji Ho Jun <129824629+ho0010@users.noreply.github.com>
2026-03-26 12:02:17 +0900

    [Silver IV] Title: 차집합, Time: 960 ms, Memory: 204732 KB -BaekjoonHub

---
+++ 백준/Silver/1822. 차집합/차집합.js
@@ -0,0 +1,15 @@
+let input = require('fs').readFileSync('./dev/stdin').toString().trim().split('\n');
+let [N, M] = input[0].split(' ').map(Number);
+let tgArr = input[1].split(' ').map(Number);
+let myArr = input[2].split(' ').map(Number);
+
+const mySet = new Set(myArr);
+
+let answer = tgArr.filter((n) => !mySet.has(n));
+
+answer.sort((a, b) => a - b);
+
+console.log(answer.length);
+if (answer.length > 0) {
+  console.log(answer.join(' '));
+}
